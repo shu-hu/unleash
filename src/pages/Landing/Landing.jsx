16e79c07b27001bea2477f001e3fff4e7279d30a
teradaian <teradaian@gmail.com>
2021-08-04 15:07:59 -0700

    text test

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -4,7 +4,7 @@ const Landing = ({user}) => {
   return (
     <main className={styles.container}>
       <h1>
-        hello, {user ? user.name : "friend"}
+        hello, {user ? user.name : "dog lover"}
       </h1>
     </main>
   )
