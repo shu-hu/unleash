bf7d1ef2bb0fa78629690f4b1751c240cdd7647b
teradaian <teradaian@gmail.com>
2021-08-09 12:08:18 -0700

    park card refactor

--- src/components/Park/ParkCard.jsx
+++ src/components/Park/ParkCard.jsx
@@ -1,20 +1,27 @@
-import React, { useState } from 'react'
-import { useLocation, useHistory } from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
+import { useHistory, useParams } from 'react-router-dom'
 import CommentSection from '../Comment/CommentSection'
 import ParkUpdateForm from '../Park/ParkUpdateForm'
-import { updatePark, deletePark } from '../../services/parkService'
+import { updatePark, deletePark, getParkById } from '../../services/parkService'
 
 const ParkCard = (props) => {
     const history = useHistory()
-    const location = useLocation()
-    const { park } = location.state
+    const id = useParams()
     const [ toggleUpdate, setToggleUpdate ] = useState(false)
+    const [ park, setPark ] = useState(null)
+
+    useEffect(() => {
+        (async() => {
+            const parkData = await getParkById(id.park_id)
+            setPark(parkData)
+        })()
+    }, [toggleUpdate])
 
     const handleUpdatePark = async (id, formData) => {
         try {
             const updatedPark = await updatePark(id, formData)
             updatedPark.added_by = props.user.profile._id
-            history.push(`/api/parks/details/${park._id}`)
+            setToggleUpdate(false)
         } catch (error) {
             throw error
         }
@@ -34,15 +41,16 @@ const ParkCard = (props) => {
     }
 
     return(
-        !toggleUpdate ? 
+        !toggleUpdate ?
+        park &&
         <div>
         <h1>{park.parkName}</h1>
         <CommentSection />
-    {/* auth */}
         <button onClick={handleClick}>Update</button>
-
+        
         </div>
         :
+        park &&
         <>
         <ParkUpdateForm park={park} handleUpdatePark={handleUpdatePark} />
         <button onClick={() => handleDeletePark(park._id)}>Delete</button>
