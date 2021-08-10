2c1fc6c47d70c0363a1716f472c823a6896d01c2
teradaian <teradaian@gmail.com>
2021-08-10 16:28:28 -0700

    allow result menu scroll

--- src/components/Layout/ResultMenu/ResultMenu.jsx
+++ src/components/Layout/ResultMenu/ResultMenu.jsx
@@ -2,7 +2,6 @@ import React, {useState, useEffect} from 'react';
 import {getPaginatedParks} from '../../../services/parkService'
 import ResultCard from './ResultCard'
 
-
 const ResultMenu = (props) => {
     const [parkList, setParkList] = useState(props.parks)
 
