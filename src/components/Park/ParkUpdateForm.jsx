b19ddac1a74ba8efbd2daa1b74b26feebe6fcbd1
teradaian <teradaian@gmail.com>
2021-08-10 12:30:01 -0700

    add material ui time pickers

--- src/components/Park/ParkUpdateForm.jsx
+++ src/components/Park/ParkUpdateForm.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import Button from '@material-ui/core/Button'
+import TextField from '@material-ui/core/TextField'
 import * as ParkFormStyles from './Park.module.css'
 
 const ParkUpdateForm = (props) => {
@@ -54,26 +55,35 @@ const ParkUpdateForm = (props) => {
                     value={description}
                     onChange={(e) => setDescription(e.target.value)}>
                 </input>
+            <div>
+                <TextField
+                        id="opens"
+                        label="Opens"
+                        type="time"
+                        value={openTime}
+                        onChange={(e) => setOpenTime(e.target.value)}
+                        InputLabelProps={{
+                        shrink: true,
+                        }}
+                        inputProps={{
+                        step: 300,
+                        }}
+                    />
 
-                <input
-                    required
-                    autoComplete='off'
-                    placeholder="OpenTime"
-                    name="openTime"
-                    value={openTime}
-                    onChange={(e) => setOpenTime(e.target.value)}>
-                </input>
-
-                <input
-                    required
-                    autoComplete='off'
-                    placeholder="CloseTime"
-                    name="closeTime"
-                    value={closeTime}
-                    onChange={(e) => setCloseTime(e.target.value)}>
-                </input>
-
-                
+                    <TextField
+                        id="closes"
+                        label="Closes"
+                        type="time"
+                        value={closeTime}
+                        onChange={(e) => setCloseTime(e.target.value)}
+                        InputLabelProps={{
+                        shrink: true,
+                        }}
+                        inputProps={{
+                        step: 300,
+                        }}
+                    />
+        </div>  
                 <div className="border"></div>
                 
                 <Button 
