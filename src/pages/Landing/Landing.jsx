145f5ec280dfebaa87282826354d7e8fb4726f36
shu-hu <shunuan.hu69@qmail.cuny.edu>
2021-08-10 16:19:01 -0400

    split landing page

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -3,15 +3,16 @@ import { Link } from 'react-router-dom'
 
 const Landing = ({ user }) => {
   return (
-    <div className="landing-page">
-      {/* hello, {user ? user.name : "dog lover"} */}
-      <div className="landing-elements">
+    <div className="landing-page-container">
+      <div className="landing-col left">
+        <h1>left</h1>
+      </div>
+      <div className="landing-col right">
         <div className="landing-button-container">
           <button><Link to="/signup">Sign Up</Link></button>
           <button><Link to="/signin">Sign In</Link></button>
         </div>
       </div>
-
     </div>
   )
 }
