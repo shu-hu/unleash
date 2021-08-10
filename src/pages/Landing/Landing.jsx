978c2fd1655a036799abf69bc60590d92c007419
shu-hu <shunuan.hu69@qmail.cuny.edu>
2021-08-10 16:37:42 -0400

    add team icon

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -1,5 +1,6 @@
 import './Landing.css'
 import { Link } from 'react-router-dom'
+import Teamlogo from '../../assets/logo/team-logo.png'
 
 const Landing = ({ user }) => {
   return (
@@ -9,6 +10,7 @@ const Landing = ({ user }) => {
       </div>
       <div className="landing-col right">
         <div className="landing-button-container">
+          <img className="team-logo-img" src={Teamlogo} alt="team-logo"></img>
           <button><Link to="/signup">Sign Up</Link></button>
           <button><Link to="/signin">Sign In</Link></button>
         </div>
