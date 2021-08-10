665febf6edfd9fed1b5dc8cc4eb98c7e972cc55b
shu-hu <shunuan.hu69@qmail.cuny.edu>
2021-08-10 17:31:02 -0400

    place team logo in middle

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -1,16 +1,17 @@
 import './Landing.css'
 import { Link } from 'react-router-dom'
 import Teamlogo from '../../assets/logo/team-logo.png'
+import Applogo from '../../assets/logo/unleash-logo-white.png'
 
 const Landing = ({ user }) => {
   return (
     <div className="landing-page-container">
       <div className="landing-col left">
-        <h1>left</h1>
+        <img className="team-logo-img" src={Teamlogo} alt="team-logo"></img>
       </div>
       <div className="landing-col right">
         <div className="landing-button-container">
-          <img className="team-logo-img" src={Teamlogo} alt="team-logo"></img>
+          <img className="app-logo-img" src={Applogo} alt="unleash-logo"></img>
           <button><Link to="/signup">Sign Up</Link></button>
           <button><Link to="/signin">Sign In</Link></button>
         </div>
