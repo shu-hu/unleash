80c4ce44c483b6ecc5a7d2ddf92112a53ea08132
shu-hu <shunuan.hu69@qmail.cuny.edu>
2021-08-10 21:09:00 -0400

    finish wleftside landing page

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -4,21 +4,34 @@ import Teamlogo from '../../assets/logo/team-logo.png'
 import Applogo from '../../assets/logo/unleash-logo-white.png'
 import Animation from '../../components/misc/Animation'
 import dogPark from '../../assets/animation/dog-park.json'
-import dogWalk from '../../assets/animation/corgi-running.json'
+import dogWalkM from '../../assets/images/dog_walking_man.svg'
+import dogWalkW from '../../assets/images/dog_walking_woman.svg'
+
 
 const Landing = ({ user }) => {
   return (
     <div className="landing-page-container">
       <div className="landing-col left">
+        <div className="landing-sign-in">
+          <Link to="/signin" style={{ textDecoration: 'none' }}>EXPLORE...</Link>
+        </div>
+
         <img className="team-logo-img" src={Teamlogo} alt="team-logo"></img>
-        <Animation animData={dogPark}></Animation>
+        <div className="landing-one">
+          <img className="dog-walk-man-img" src={dogWalkM} alt="dog-walk-man"></img>
+        </div>
+        <div className="animation-two">
+          <Animation animData={dogPark}></Animation>
+        </div>
+        <div className="landing-three">
+          <img className="dog-walk-woman-img" src={dogWalkW} alt="dog-walk-woman"></img>
+        </div>
+
       </div>
       <div className="landing-col right">
         <div className="landing-button-container">
           <img className="app-logo-img" src={Applogo} alt="unleash-logo"></img>
           <button><Link to="/signup">Sign Up</Link></button>
-          <button><Link to="/signin">Sign In</Link></button>
-          <Animation animData={dogWalk}></Animation>
         </div>
       </div>
     </div>
