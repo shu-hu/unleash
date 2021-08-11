95a7d29a2904e93b126fd281ad1fef286d015495
shu-hu <shunuan.hu69@qmail.cuny.edu>
2021-08-11 16:20:03 -0400

    edit div classname in login form

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { Link, useHistory } from 'react-router-dom'
-import styles from './LoginForm.module.css'
+import './LoginForm.css'
 
 import { login } from '../../services/authService'
 
@@ -19,65 +19,70 @@ const LoginForm = ({ handleSignupOrLogin }) => {
   const handleSubmit = async evt => {
     evt.preventDefault()
     try {
-        await login(formData)
-        handleSignupOrLogin()
-        history.push('/')
+      await login(formData)
+      handleSignupOrLogin()
+      history.push('/')
     } catch (error) {
-        setAuthError(error.message)
-        setFormData({
-          email: '',
-          password: '',
-        })
+      setAuthError(error.message)
+      setFormData({
+        email: '',
+        password: '',
+      })
     }
   }
 
   return (
-    <div>
-      <h1>Login</h1>
-      {!authError ? 
-      <h3>Enter your login information</h3>
+    <div className='form-container'>
+      <div className="title-container">
+        <h1>LogIn !</h1>
+      </div>
+
+      {!authError ?
+        <h3>Enter your login information</h3>
         :
-      {authError}
-    }
-    <form
-      autoComplete="off"
-      onSubmit={handleSubmit}
-    >
-      <div className={styles.inputContainer}>
-        <label htmlFor="email-input" className={styles.label}>
-          Email
-        </label>
-        <input
-          type="text"
-          autoComplete="off"
-          id="email-input"
-          value={formData.email}
-          name="email"
-          onChange={handleChange}
-        />
-      </div>
-      <div className={styles.inputContainer}>
-        <label htmlFor="password-input" className={styles.label}>
-          Password
-        </label>
-        <input
-          type="password"
-          autoComplete="off"
-          id="password-input"
-          value={formData.password}
-          name="password"
-          onChange={handleChange}
-        />
-      </div>
-      <div>
-        <button>Log In</button>
-        <Link to="/">
-          <button>Cancel</button>
-        </Link>
-      </div>
-    </form>
+        { authError }
+      }
+
+      <form
+        autoComplete="off"
+        onSubmit={handleSubmit}
+        className="register-form"
+      >
+        <div>
+          <label htmlFor="email-input" className="label">
+            Email
+          </label>
+          <input
+            type="text"
+            autoComplete="off"
+            id="email-input"
+            value={formData.email}
+            name="email"
+            onChange={handleChange}
+          />
+        </div>
+        <div className="inputContainer">
+          <label htmlFor="password-input" className="label">
+            Password
+          </label>
+          <input
+            type="password"
+            autoComplete="off"
+            id="password-input"
+            value={formData.password}
+            name="password"
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <button id="submit-button" type="submit">LOGIN</button>
+          <Link to="/">
+            <button id="cancel-button" type="submit">CANCEL</button>
+          </Link>
+        </div>
+      </form>
     </div>
   )
 }
- 
+
 export default LoginForm
