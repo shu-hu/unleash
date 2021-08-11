12aade62d6bbd5f7eaac7e9985d87128e4fb3994
shu-hu <shunuan.hu69@qmail.cuny.edu>
2021-08-11 16:58:55 -0400

    finish sign in page

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,14 +1,23 @@
 import React from 'react'
 import LoginForm from '../../components/LoginForm/LoginForm'
-import styles from './Login.module.css'
+import './Login.css'
+import Animation from '../../components/misc/Animation'
+import rightSide from '../../assets/animation/user-profile.json'
 
 const LoginPage = ({ handleSignupOrLogin }) => {
   return (
-    <main className={styles.container}>
-      <h1>Log In!</h1>
-      <LoginForm handleSignupOrLogin={handleSignupOrLogin} />
+    <main className="signup-page">
+      <div className="left-container">
+        <div className="animation-box">
+          <Animation animData={rightSide}></Animation>
+        </div>
+      </div>
+
+      <div className="right-container">
+        <LoginForm handleSignupOrLogin={handleSignupOrLogin} />
+      </div>
     </main>
   )
 }
- 
+
 export default LoginPage
