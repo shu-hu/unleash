aaec923c371a87ff6bfc06ceb72af3f0eb7814e8
teradaian <teradaian@gmail.com>
2021-08-12 07:17:12 -0700

    update park card style

--- src/components/Park/ParkUpdateForm.jsx
+++ src/components/Park/ParkUpdateForm.jsx
@@ -1,7 +1,11 @@
 import React, { useState } from 'react'
 import Button from '@material-ui/core/Button'
+import Box from '@material-ui/core/Box'
+import Typography from '@material-ui/core/Typography'
 import TextField from '@material-ui/core/TextField'
 import * as ParkFormStyles from './Park.module.css'
+
+import EditLocationIcon from '@material-ui/icons/EditLocation';
 
 const ParkUpdateForm = (props) => {
     const [parkName, setParkName] = useState(props.park.parkName)
@@ -23,78 +27,97 @@ const ParkUpdateForm = (props) => {
     }
 
         return (
-            <form  className={[ParkFormStyles.createForm]} onSubmit={handleSubmit}>
-
-                <div className="add-park-prompt">
-                        <label>Update Park</label>
-                </div>
-                
-                <input
-                    required
-                    autoComplete='off'
-                    placeholder="Park Name"
-                    name="parkName"
-                    value={parkName}
-                    onChange={(e) => setParkName(e.target.value)}>
-                </input>
-
-                <input
-                    required
-                    autoComplete='off'
-                    placeholder="Address"
-                    name="address"
-                    value={address}
-                    onChange={(e) => setAddress(e.target.value)}>
-                </input>
-
-                <input
-                    required
-                    autoComplete='off'
-                    placeholder="Description"
-                    name="description"
-                    value={description}
-                    onChange={(e) => setDescription(e.target.value)}>
-                </input>
-            <div>
-                <TextField
-                        id="opens"
-                        label="Opens"
-                        type="time"
-                        varient="outlined"
-                        value={openTime}
-                        onChange={(e) => setOpenTime(e.target.value)}
-                        InputLabelProps={{
-                        shrink: true,
-                        }}
-                        inputProps={{
-                        step: 300,
-                        }}
-                    />
-
+            <form className={ParkFormStyles.createForm} onSubmit={handleSubmit}>
+                <Box 
+                    className={ParkFormStyles.inputContainer} 
+                    boxShadow={2}
+                    display="flex" 
+                    flexDirection="column"
+                    width={500}
+                >
+                    <Box m='auto'>
+                        <Typography variant="h4" gutterBottom style={{ color: '#121212'}}>
+                            Update Park <EditLocationIcon fontSize='large' color='primary' />
+                        </Typography>
+                    </Box>
+                    <div className={ParkFormStyles.border}></div>
+                    <Box m={2}></Box>
+                    
                     <TextField
-                        id="closes"
-                        label="Closes"
-                        type="time"
-                        varient="outlined"
-                        value={closeTime}
-                        onChange={(e) => setCloseTime(e.target.value)}
-                        InputLabelProps={{
-                        shrink: true,
-                        }}
-                        inputProps={{
-                        step: 300,
-                        }}
-                    />
-        </div>  
-                <div className="border"></div>
-                
-                <Button 
-                    variant="contained" 
-                    color="primary" 
-                    type="submit">
-                Submit
-                </Button>
+                        required
+                        fullWidth
+                        variant="outlined"
+                        autoComplete='off'
+                        label="Park Name"
+                        name="parkName"
+                        value={parkName}
+                        onChange={(e) => setParkName(e.target.value)}>
+                    </TextField>
+                    <Box m={1}></Box>
+                    <TextField
+                        required
+                        fullWidth
+                        variant="outlined"
+                        label="Park Address"
+                        autoComplete='off'
+                        name="address"
+                        value={address}
+                        onChange={(e) => setAddress(e.target.value)}>
+                    </TextField>
+                    <Box m={1}></Box>
+                    <TextField
+                        required
+                        fullWidth
+                        multiline
+                        rows={3}
+                        variant="outlined"
+                        label="Description"
+                        autoComplete='off'
+                        name="description"
+                        value={description}
+                        onChange={(e) => setDescription(e.target.value)}>
+                    </TextField>
+                    <Box m={1}></Box>
+                <div>
+                    <TextField
+                            id="opens"
+                            label="Opens"
+                            type="time"
+                            varient="outlined"
+                            value={openTime}
+                            onChange={(e) => setOpenTime(e.target.value)}
+                            InputLabelProps={{
+                            shrink: true,
+                            }}
+                            inputProps={{
+                            step: 300,
+                            }}
+                        />
 
+                        <TextField
+                            id="closes"
+                            label="Closes"
+                            type="time"
+                            varient="outlined"
+                            value={closeTime}
+                            onChange={(e) => setCloseTime(e.target.value)}
+                            InputLabelProps={{
+                            shrink: true,
+                            }}
+                            inputProps={{
+                            step: 300,
+                            }}
+                        />
+                </div>  
+                    <Box m={2}></Box>
+                    <Button 
+                        fullWidth
+                        variant="contained" 
+                        color="primary" 
+                        type="submit">
+                    Submit
+                    </Button>
+                </Box>
             </form>
     )
 }
